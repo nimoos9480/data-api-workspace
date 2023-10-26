4e9ea6778b35ddeeb5f631074746c8687e97222b
Shin soomin <soomin6372@gmail.com>
2023-10-26 10:21:48 +0900

    feat_시험관련

---
+++ Exam3(1)/src/com/kh/DAO/MemberDAO.java
@@ -0,0 +1,31 @@
+package com.kh.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+
+	public Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "kh";
+		String pw = "kh";
+		
+		return DriverManager.getConnection(url, id, pw);
+	}
+	
+	public boolean login(String id, String pwd) throws Exception {
+		String sql = "select id from members where id=? and pwd=?";
+		
+		Connection conn = this.getConnection();
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1, id);
+		pstmt.setString(2, pwd);
+		ResultSet rs = pstmt.executeQuery();
+		
+		return rs.next(); // 결과가 1개라도 있으면 true반환하니 바로 리턴받는다.
+	}
+}
